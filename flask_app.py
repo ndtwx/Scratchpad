1fef54ee71af5d5018713b28316be4373644473c
nd_twx@hotmail.com <woshiiandy@4146c987a9a6>
2023-07-24 14:11:47 +0000

    Replaced dummy comments with some in-memory storage

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,21 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+comments = []
 app.config["DEBUG"] = True
 
-@app.route("/")
+#Specifies that the following function defines the view for the “/” URL, and that it accepts both “GET” and “POST” requests.
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("main_page.html")
-
+    #If the request we’re currently processing is a “GET” one, the viewer just wants to see the page, so we render the template. We’ve added an extra parameter to the call to render_template – the comments=comments bit. This means that the list of comments that we’ve defined as a variable further up will be available inside our template; we’ll update that to use the list in a moment.
+    #If the request isn’t a “GET” (and so, we know it’s a “POST” – someone’s clicked the “Post comment” button) then the next bit is executed:
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
+    #This extracts the stuff that was typed into the textarea in the page from the browser’s request; we know it’s in a thing called contents because that was the name we gave the textarea in the template:
+    #<textarea class="form-control" name="contents" placeholder="Enter a comment"></textarea>
+    comments.append(request.form["contents"])
+    #Once we’ve extracted the comment contents from the request, we add it to the list. Finally, once that’s been stored, we send a message back to the browser saying “Please request this page again, this time using a ‘GET’ method”, so that the user can see the results of their post:
+    return redirect(url_for('index'))
