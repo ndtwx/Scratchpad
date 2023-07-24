72d8ce3e9161fe04b3820798d60b6324d4d47919
nd_twx@hotmail.com <woshiiandy@4146c987a9a6>
2023-07-24 16:30:27 +0000

    make flask app connect to database

--- flask_app.py
+++ flask_app.py
@@ -5,7 +5,6 @@ from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-comments = []
 app.config["DEBUG"] = True
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
@@ -17,7 +16,17 @@ SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostnam
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db = SQLAlchemy(app)
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+
+comments = []
 
 #Specifies that the following function defines the view for the “/” URL, and that it accepts both “GET” and “POST” requests.
 @app.route("/", methods=["GET", "POST"])
