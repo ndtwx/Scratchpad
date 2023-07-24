79865db25ef0ede4227b64c5933b42612e8513e1
nd_twx@hotmail.com <woshiiandy@4146c987a9a6>
2023-07-24 17:37:28 +0000

    add in log in and log out option

--- flask_app.py
+++ flask_app.py
@@ -3,7 +3,7 @@
 
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import login_user, LoginManager, UserMixin
+from flask_login import login_user, LoginManager, UserMixin, logout_user, login_required
 from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
@@ -87,4 +87,8 @@ def login():
     login_user(user)
     return redirect(url_for('index'))
     
-
+@app.route("/logout/")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
