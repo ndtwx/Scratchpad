92e35cac667bd3dce10acfde6c3a74c59f759212
AndyTang <nd_twx@hotmail.com>
2023-08-01 14:33:04 +0000

    remove portfolio

--- flask_app.py
+++ flask_app.py
@@ -97,7 +97,3 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-#@app.route("/portfolio")
-#def portfolio():
-    # You can define your portfolio data and pass it to the template here
-    #return render_template('portfolio_page.html')
